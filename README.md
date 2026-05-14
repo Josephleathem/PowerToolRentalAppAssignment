7e78469c1b38dc17eca30c8c6cbb2b1bfd31e056
Josephleathem <Jleat2620@gmail.com>
2026-05-14 02:44:23 -0400

    Finalized the app. Created the README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Week 3: Hands On - Power Tool Rental App
+
+
+## Learning Objectives
+
+CO4 Create and customize an Android project with icons and interactive components, such as RadioButtons, applying layout and alignment attributes, implementing conditional statements for decision-making, and displaying notifications.
+
+## Assignment Prompt
+
+Design a Power Tool Rental App. The app determines the cost of a power washer or tiller. The opening screen requests the number of days that the power tool will be rented.
+
+The user selects a radio button labeled Power Washer or Tiller and then selects the Compute Cost button.
+
+The final cost is displayed for the number of days rented.
+
+The result is rounded off to the nearest penny.
+
+The power washer costs $55.99 a day and the tiller costs $68.99 a day.
+
+Do not enter more than seven days.
+
+Locate an image online and resize it for use as a custom launcher icon and Action bar icon.
+
+## Assignment Submission
+
+For each program, attach the two required files to your submission:
+
+1. Copy/paste your code and output screenshots into a Microsoft Word file.
+2. Your source files.
--- app/src/main/java/com/example/powertoolrental/MainActivity.java
+++ app/src/main/java/com/example/powertoolrental/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.powertoolrental;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
+    double washerCost = 55.99;
+    double tillerCost = 68.99;
+    double daysEntered;
+    double total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +24,36 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.mipmap.ic_launcher);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        final EditText days = (EditText) findViewById(R.id.tvDays);
+        final RadioButton rbWasher = (RadioButton) findViewById(R.id.rbWasher);
+        final RadioButton rbTiller = (RadioButton) findViewById(R.id.rbTiller);
+        final TextView result = (TextView) findViewById(R.id.tvResult);
+        Button calculate = (Button) findViewById(R.id.btCalculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                daysEntered = Double.parseDouble(days.getText().toString());
+                DecimalFormat currency = new DecimalFormat("$###,###.##");
+                if (rbWasher.isChecked()) {
+                    if (daysEntered <= 7) {
+                        total = washerCost * daysEntered;
+                        result.setText("Total Cost: " + currency.format(total));
+                    } else {
+                        Toast.makeText(MainActivity.this, "You can not rent for more than 7 days", Toast.LENGTH_LONG).show();
+                        result.setText("");
+                    }
+                }
+                if (rbTiller.isChecked()) {
+                    if (daysEntered <= 7) {
+                        total = tillerCost * daysEntered;
+                        result.setText("Total Cost: " + currency.format(total));
+                    } else {
+                        Toast.makeText(MainActivity.this, "You can not rent for more than 7 days", Toast.LENGTH_LONG).show();
+                        result.setText("");
+                    }
+                }
+            }
+        });
     }
 }
